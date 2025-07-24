bff22c959a3d987247f6a54837eb56c7096d18a5
jhk01007 <jhk001007@naver.com>
2025-07-24 10:47:36 +0900

    Stomp Chat 화면 구성

---
+++ src/views/StompChatPage.vue
@@ -0,0 +1,99 @@
+<template>
+    <v-container>
+        <v-row justify="center">
+            <v-col cols="12" md="8">
+                <v-card>
+                    <v-card-titile class="text-center text-h5">
+                        채팅
+                    </v-card-titile>
+                    <v-card-text>
+                        <div class="chat-box">
+                            <div 
+                            v-for="(msg, index) in messages"
+                            :key="index"
+                            >
+                                {{ msg }}
+                            </div>
+                        </div>
+                        <v-text-field
+                            v-model="newMessage"
+                            label="메시지 입력"
+                            @keyup.enter="sendMessage"
+                        />
+                        <v-btn color="primary" block @click="sendMessage">전송</v-btn>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import SockJS from 'sockjs-client';
+import Stomp from 'webstomp-client';
+// import axios from 'axios';
+
+
+    export default {
+        data() {
+            return {
+                // ws: null,
+                messages: [],
+                newMessage: "",
+                stompClient: null
+            }
+        },
+        created() {
+            this.connectWebSocket();
+        },
+        beforeUnmount() {
+            this.disconnectWebSocket()
+        },
+        methods: {
+            connectWebSocket() {
+                // sockJs는 websocket을 내장한 향상된 js 라이브러리. http 엔드포인트 사용.
+                const sockJs = new SockJS(`${process.env.VUE_APP_API_BASE_URL}/connect`)
+                this.stompClient = Stomp.over(sockJs);
+
+                this.stompClient.connect({},
+                    () => {
+                        this.stompClient.subscribe(`/topic/1`, (message) => {
+                            console.log(message)
+                            this.messages.push(message.body);
+                            this.scrollToBottom();
+                        })
+                    }
+                )
+            },
+
+            sendMessage() {
+                if(this.newMessage.trim() == "") return;
+                this.stompClient.send(`/publish/1`, this.newMessage);
+                this.newMessage = ""
+            },
+
+            scrollToBottom() {
+                this.$nextTick(() => {
+                    const chatBox = this.$el.querySelector(".chat-box");
+                    chatBox.scrollTop = chatBox.scrollHeight;
+                })
+            },
+
+            disconnectWebSocket() {
+                // if(this.ws) {
+                    // this.ws.close();
+                    // console.log("disconnected!!");
+                    // this.ws = null;
+                // }
+            }
+        }
+    }
+</script>
+<style>
+.chat-box{
+    height: 300px;
+    overflow-y: auto;
+    border: 1px solid #ddd;
+    margin-bottom: 10px;
+}
+</style>
